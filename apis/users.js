5f03b6ba45251613b0b58733ebcc631852774047
sungjunyoung <wnsdud1861@gmail.com>
2017-06-04 15:45:44 +0900

    로그인 / 회원가입 로직 구현 완료

--- apis/users.js
+++ apis/users.js
@@ -7,9 +7,9 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const moment = require('moment');
 
-const mysql = require('mysql');
+const mysql = require('mysql-promise')();
 const mysqlConfig = require('../config/mysql.json');
-const connection = mysql.createConnection(mysqlConfig);
+mysql.configure(mysqlConfig);
 
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({extended: false}));
@@ -35,22 +35,86 @@ router.post('/', function (req, res, next) {
         userInfo.username = req.body.name;
         userInfo.profile_url = req.body.picture.data.url;
         userInfo.password = req.body.accessToken;
+        userInfo.fb_id = req.body.id;
         userInfo.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
         userInfo.login_type = req.body.loginType;
 
-        // TODO 디비 중복체크
-        connection.query('INSERT INTO gj_users SET ?', userInfo, function (err, rows) {
-            if (err) {
-                res.status(500);
-                res.send(err);
-            } else {
+        // 디비 확인 후 해다 유저가 이미 있으면 UPDATE, 없으면 INSERT
+        mysql.query('SELECT * FROM gj_users WHERE ' +
+            'login_type = \'' + userInfo.login_type + '\' AND ' +
+            'fb_id = \'' + userInfo.fb_id + '\'')
+            .spread(function (rows) {
+                if (rows.length > 0) {
+                    // 유저가 이미 로그인했다면
+                    return mysql.query('UPDATE gj_users SET ? ' +
+                        'WHERE password = \'' + userInfo.password + '\'', userInfo)
+                } else {
+                    // 새로 등록하는 유저라면
+                    return mysql.query('INSERT INTO gj_users SET ?', userInfo)
+                }
+            })
+            .then(function () {
                 res.status(201);
-                res.send('유저 생성 완료');
-            }
-
-        })
+                res.json({code: 'SUCCESS', message: '페이스북 로그인/회원가입 완료'})
+            })
+            .catch(function (err) {
+                res.status(500);
+                res.json({code: 'DB_ERR', message: '데이터베이스 에러'});
+            })
     } else {
-        res.status(500).send('미구현');
+        // 일반 로그인일 때
+
+        // 예외처리
+        if (req.body.username.length < 10) {
+            res.status(400);
+            res.json({code: 'ID_LENGTH', message: '닉네임 길이제한 10자'});
+        }
+        if (req.body.password.length < 8) {
+            res.status(400);
+            res.json({code: 'PW_LENGTH', message: '패스워드 길이제한 8자'});
+        }
+
+        userInfo.username = req.body.username;
+        userInfo.profile_url = '';
+        userInfo.password = req.body.password;
+        userInfo.fb_id = '-';
+        userInfo.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+        userInfo.login_type = 'default';
+
+        mysql.query('SELECT * FROM gj_users WHERE ' +
+            'login_type = \'' + userInfo.login_type + '\' AND ' +
+            'username = \'' + userInfo.username + '\'')
+            .spread(function (rows) {
+                if (rows.length > 0) {
+                    // 유저 닉네임 존재
+                    if(rows[0].password === req.body.password){
+                        // 패스워드도 일치
+                        return true;
+                    } else {
+                        // 패스워드 불일치
+                        throw 'PW_INVAILD';
+                    }
+                } else {
+                    // 새로 등록하는 유저
+                    return mysql.query('INSERT INTO gj_users SET ?', userInfo)
+                }
+            })
+            .then(function () {
+                res.status(201);
+                res.json({code: 'SUCCESS', message: '로그인/회원가입 완료'});
+            })
+            .catch(function (err) {
+                console.log(err);
+                if(err === 'PW_INVAILD'){
+                    res.status(400);
+                    res.json({code: err, message: '패스워드 불일치'});
+                } else {
+                    res.status(500);
+                    res.json({code: 'DB_ERR', message: '데이터베이스 에러'});
+                }
+
+            });
+
     }
 
 });
--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -10,17 +10,19 @@ import FacebookLogin from 'react-facebook-login';
 
 export default class Auth extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             formHeight: 0,
             formWidth: 0,
+            userId: '',
+            userPw: ''
         }
     }
 
-    componentDidMount(){
-        let { clientHeight, clientWidth } = this.refs.form;
-        this.setState({formHeight: clientHeight, formWidth:clientWidth})
+    componentDidMount() {
+        let {clientHeight, clientWidth} = this.refs.form;
+        this.setState({formHeight: clientHeight, formWidth: clientWidth})
     }
 
     responseFacebook(response) {
@@ -33,49 +35,86 @@ export default class Auth extends Component {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(response)
-            }).then((response) => console.log(response));
+            }).then((response) => response.json())
+                .then((response) => {
+                    console.log(response)
+                });
         } else {
 
         }
     }
 
+    onIdChange(e) {
+        this.setState({userId: e.target.value})
+    }
+
+    onPwChange(e) {
+        this.setState({userPw: e.target.value})
+    }
+
+    onLogin() {
+        const id = this.state.userId;
+        const pw = this.state.userPw;
+
+        const userInfo = {};
+        userInfo.username = id;
+        userInfo.password = pw;
+        userInfo.loginType = 'default';
+        fetch('/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfo)
+        }).then((response) => response.json())
+            .then((response) => {
+                console.log(response)
+            });
+    }
+
     render() {
         return (
             <div className="auth">
-                <div ref="form" style={{display: 'inline-block', position: 'absolute',
-                    top: '50%',left: '50%',
-                    marginTop: - this.state.formHeight/2, marginLeft: -this.state.formWidth/2}}>
-                <Card style={{maxWidth: 400, margin: 'auto', padding: 30}}>
-                    <CardTitle title="겟짤" subtitle="퍼가용"/>
-                    <CardText>
-                        자신만의 짤방을 수집하고 다른 사람들과 공유해보세요!
-                    </CardText>
-                    <CardActions style={{width: 300, margin: 'auto'}}>
-                        <TextField
-                            style={{width: 300}}
-                            hintText="아이디를 입력해주세요"
-                            floatingLabelText="아이디"
-                        />
-                        <TextField
-                            style={{width: 300, marginTop: -10}}
-                            hintText="패스워드를 입력해주세요"
-                            floatingLabelText="패스워드"
-                            type="password"
-                        />
-                        <RaisedButton
-                            style={{width: 300, marginTop: 10}}
-                            label="로그인"/>
+                <div ref="form" style={{
+                    display: 'inline-block', position: 'absolute',
+                    top: '50%', left: '50%',
+                    marginTop: -this.state.formHeight / 2, marginLeft: -this.state.formWidth / 2
+                }}>
+                    <Card style={{maxWidth: 400, margin: 'auto', padding: 30}}>
+                        <CardTitle title="겟짤" subtitle="퍼가용"/>
+                        <CardText>
+                            자신만의 짤방을 수집하고 다른 사람들과 공유해보세요!
+                        </CardText>
+                        <CardActions style={{width: 300, margin: 'auto'}}>
+                            <TextField
+                                style={{width: 300}}
+                                onChange={this.onIdChange.bind(this)}
+                                hintText="아이디를 입력해주세요"
+                                floatingLabelText="아이디"
+                            />
+                            <TextField
+                                style={{width: 300, marginTop: -10}}
+                                onChange={this.onPwChange.bind(this)}
+                                hintText="패스워드를 입력해주세요"
+                                floatingLabelText="패스워드"
+                                type="password"
+                            />
+                            <RaisedButton
+                                onTouchTap={this.onLogin.bind(this)}
+                                style={{width: 300, marginTop: 10}}
+                                label="로그인 / 회원가입"/>
 
-                        <FacebookLogin
-                            appId="1922219037991500"
-                            autoLoad={true}
-                            fields="name,email,picture"
-                            cssClass="facebook-login"
-                            textButton="페이스북으로 로그인"
-                            callback={this.responseFacebook}
-                        />
-                    </CardActions>
-                </Card>
+                            <FacebookLogin
+                                appId="1922219037991500"
+                                autoLoad={true}
+                                fields="name,email,picture"
+                                cssClass="facebook-login"
+                                textButton="페이스북으로 로그인"
+                                callback={this.responseFacebook}
+                            />
+                        </CardActions>
+                    </Card>
                 </div>
             </div>
         );
