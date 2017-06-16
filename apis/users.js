699c9f8bf4ce2c2aa1043b606105e5ee48df398e
sungjunyoung <wnsdud1861@gmail.com>
2017-06-16 11:33:32 +0900

    회원가입시 닉네임길이 패스워드길이 안맞으면 커넥션 end - 리턴

--- apis/users.js
+++ apis/users.js
@@ -29,7 +29,7 @@ router.get('/:userId', function (req, res, next) {
 });
 
 // 유저가 좋아요 한 짤방 얻기
-router.get('/:userId/jjals/like', function(req,res,next){
+router.get('/:userId/jjals/like', function (req, res, next) {
     var userId = req.params.userId;
 
     var page = req.query.page - 1;
@@ -37,7 +37,7 @@ router.get('/:userId/jjals/like', function(req,res,next){
 
     mysql.query('SELECT gj_jjals.* FROM gj_user_likes JOIN gj_jjals ' +
         'ON gj_jjals.id = gj_user_likes.jjal_id WHERE gj_user_likes.user_id = ? ORDER BY created_at DESC LIMIT ' + pagingQuery, userId)
-        .spread(function(rows){
+        .spread(function (rows) {
             res.status(200);
             res.json({likeJjals: rows})
         })
@@ -138,65 +138,68 @@ router.post('/', function (req, res, next) {
     } else {
         // 일반 로그인일 때
 
+        let inError = false;
         // 예외처리
         if (req.body.username.length < 6) {
             res.status(400);
             res.json({code: 'ID_LENGTH', message: '닉네임 길이제한 6자'});
             res.end();
+            inError = true;
         }
         if (req.body.password.length < 8) {
             res.status(400);
             res.json({code: 'PW_LENGTH', message: '패스워드 길이제한 8자'});
             res.end();
+            inError = true;
         }
 
-        userInfo.username = req.body.username;
-        userInfo.profile_url = '';
-        userInfo.password = req.body.password;
-        userInfo.fb_id = '-';
-        userInfo.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
-        userInfo.login_type = 'default';
+        if (!inError) {
+            userInfo.username = req.body.username;
+            userInfo.profile_url = '';
+            userInfo.password = req.body.password;
+            userInfo.fb_id = '-';
+            userInfo.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+            userInfo.login_type = 'default';
 
-        mysql.query('SELECT * FROM gj_users WHERE ' +
-            'login_type = \'' + userInfo.login_type + '\' AND ' +
-            'username = \'' + userInfo.username + '\'')
-            .spread(function (rows) {
-                if (rows.length > 0) {
-                    // 유저 닉네임 존재
-                    if (rows[0].password === req.body.password) {
-                        // 패스워드도 일치
-                        return true;
+            mysql.query('SELECT * FROM gj_users WHERE ' +
+                'login_type = \'' + userInfo.login_type + '\' AND ' +
+                'username = \'' + userInfo.username + '\'')
+                .spread(function (rows) {
+                    if (rows.length > 0) {
+                        // 유저 닉네임 존재
+                        if (rows[0].password === req.body.password) {
+                            // 패스워드도 일치
+                            return true;
+                        } else {
+                            // 패스워드 불일치
+                            throw 'PW_INVALID';
+                        }
                     } else {
-                        // 패스워드 불일치
-                        throw 'PW_INVALID';
+                        // 새로 등록하는 유저
+                        return mysql.query('INSERT INTO gj_users SET ?', userInfo)
                     }
-                } else {
-                    // 새로 등록하는 유저
-                    return mysql.query('INSERT INTO gj_users SET ?', userInfo)
-                }
-            })
-            .then(function () {
-                return mysql.query('SELECT id FROM gj_users WHERE username = ? AND password = ?',
-                    [userInfo.username, userInfo.password])
-            })
-            .spread(function (rows) {
-                res.status(201);
-                res.json({code: 'SUCCESS', message: '로그인/회원가입 완료', userId: rows[0].id});
-            })
-            .catch(function (err) {
-                if (err === 'PW_INVALID') {
-                    res.status(400);
-                    res.json({code: err, message: '패스워드 불일치'});
-                } else {
-                    console.log(err);
-                    res.status(500);
-                    res.json({code: 'DB_ERR', message: '데이터베이스 에러'});
-                }
+                })
+                .then(function () {
+                    return mysql.query('SELECT id FROM gj_users WHERE username = ? AND password = ?',
+                        [userInfo.username, userInfo.password])
+                })
+                .spread(function (rows) {
+                    res.status(201);
+                    res.json({code: 'SUCCESS', message: '로그인/회원가입 완료', userId: rows[0].id});
+                })
+                .catch(function (err) {
+                    if (err === 'PW_INVALID') {
+                        res.status(400);
+                        res.json({code: err, message: '패스워드 불일치'});
+                    } else {
+                        console.log(err);
+                        res.status(500);
+                        res.json({code: 'DB_ERR', message: '데이터베이스 에러'});
+                    }
 
-            });
-
+                });
+        }
     }
-
 });
 
 module.exports = router;
